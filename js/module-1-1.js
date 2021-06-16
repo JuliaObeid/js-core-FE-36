98edcdff9b9c06a4b75a250374c376b6c69673e4
JuliaObeid <78361535+JuliaObeid@users.noreply.github.com>
2021-06-17 02:28:23 +0300

    переменные

---
+++ js/module-1-1.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let a = 0
+console.log("значение переменной а:", a);
+const b = 1
+console.log("значение переменной b:", b);
+
+let c
+console.log("значение переменной c:", c);
+c = "my let variable"
+console.log("значение переменной c:", c);
